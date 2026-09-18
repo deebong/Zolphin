7f64dfda27ef92a962ac00d32600f9ae20c30e70
Rajkumar Jeyabal <112155993+deebong@users.noreply.github.com>
2026-09-18 12:35:23 +0530

    feat: update about section and clean up build scripts
    Refactor the engineering profile layout in the about section, improve grid responsiveness, and remove legacy automation utility scripts.

--- update_h1.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let html = fs.readFileSync('index.html', 'utf8');
-
-html = html.replace(/HAVE AN AZURE PROBLEM\? LET'S ENGINEER THE RIGHT PATH\./, "HAVE AN AZURE PROBLEM?<br>LET'S ENGINEER THE<br>RIGHT PATH.");
-
-fs.writeFileSync('index.html', html);
--- update_profile_css.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('styles.css', 'utf8');
-
-css = css.replace(/\.profile-list li/g, '.profile-group');
-fs.writeFileSync('styles.css', css);
